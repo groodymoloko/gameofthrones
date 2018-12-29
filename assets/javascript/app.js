838e75555ad740135e6afcdb44240cce4b5da7fb
Jenae <Jenaenay15@yahoo.com>
2018-12-29 12:13:36 -0700

    added char objects and pictures

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // load document before starting javascript
-$(document).ready(function(){
+$(document).ready(function () {
 
     // Initialize Firebase
     var config = {
@@ -12,24 +12,14 @@ $(document).ready(function(){
     };
     firebase.initializeApp(config);
     var database = firebase.database();
-    var logCharacter = "";
 
+    // variable to store ajax call URL based on quote text
+    var uri = "";
 
+    //hides page content until user selects something
+    $(".row").hide();
 
-    // Character Name	Suggested Search Parameter
-
-    // Bronn	        bronn
-    // Brynden Tully	brynden
-    // Cersei	        cersei
-    // The Hound	    hound
-    // Jaime Lannister	jaime
-    // Littlefinger	    littlefinger
-    // Olenna Tyrell	olenna
-    // Renly Baratheon	renly
-    // Tyrion	        tyrion
-    // Varys	        varys
-
-    //create list of characters with properties
+    //create list of characters with properties (some characters will be in the quote database, others are local)
     var characters = {
         "bronn": {
             name: "Bronn",
@@ -45,7 +35,7 @@ $(document).ready(function(){
             image: "assets/images/CerseiLannister.png"
         },
         "hound": {
-            name: "The Hound",
+            name: "Sandor  'The Hound'  Clegane",
             image: "assets/images/sandorclegane1.jpg"
         },
         "jaime": {
@@ -71,60 +61,234 @@ $(document).ready(function(){
         "varys": {
             name: "Lord Varys",
             image: "assets/images/Varys.png"
-        }
-        
+        },
+        "ned": {
+            name: "Eddard  'Ned'  Stark",
+            image: "assets/images/ned.jpg"
+        },
+        "robert": {
+            name: "Robert Baratheon",
+            image: "assets/images/robert.jpg"
+        },
+        "catelyn": {
+            name: "Catelyn Stark",
+            image: "assets/images/catelyn.jpg"
+        },
+        "daenerys": {
+            name: "Daenerys",
+            image: "assets/images/daenerys.jpg"
+        },
+        "jorah": {
+            name: "Jorah Mormont",
+            image: "assets/images/jorah.jpg"
+        },
+        "viserys": {
+            name: "Viserys Targaryen",
+            image: "assets/images/viserys.jpg"
+        },
+        "sansa": {
+            name: "Sansa Stark",
+            image: "assets/images/sansa.jpg"
+        },
+        "arya": {
+            name: "Arya Stark",
+            image: "assets/images/arya.jpg"
+        },
+        "robb": {
+            name: "Robb Stark",
+            image: "assets/images/robb.jpg"
+        },
+        "theon": {
+            name: "Theon Greyjoy",
+            image: "assets/images/theon.jpg"
+        },
+        "bran": {
+            name: "Bran Stark",
+            image: "assets/images/bran.jpg"
+        },
+        "joffrey": {
+            name: "Joffrey Baratheon",
+            image: "assets/images/joffrey.jpg"
+        },
+        "davos": {
+            name: "Davos Seaworth",
+            image: "assets/images/davos.jpg"
+        },
+        "samwell": {
+            name: "Samwell Tarly",
+            image: "assets/images/samwell.jpg"
+        },
+        "stannis": {
+            name: "Stannis Baratheon",
+            image: "assets/images/stannis.jpg"
+        },
+        "melisandre": {
+            name: "Melisandre",
+            image: "assets/images/melisandre1.jpg"
+        },
+        "jeor": {
+            name: "Jeor Mormont",
+            image: "assets/images/jeor.jpg"
+        },
+        "shae": {
+            name: "Shae",
+            image: "assets/images/shae.jpg"
+        },
+        "margaery": {
+            name: "Margaery Tyrell",
+            image: "assets/images/margaery.jpg"
+        },
+        "tywin": {
+            name: "Tywin Lannister",
+            image: "assets/images/tywin.jpg"
+        },
+        "talisa": {
+            name: "Talisa Maegyr",
+            image: "assets/images/talisa.jpg"
+        },
+        "ygritte": {
+            name: "Ygritte",
+            image: "assets/images/ygritte.jpg"
+        },
+        "gendry": {
+            name: "Gendry",
+            image: "assets/images/gendry.jpg"
+        },
+        "tormund": {
+            name: "Tormund Giantsbane",
+            image: "assets/images/tormund.jpg"
+        },
+        "brienne": {
+            name: "Brienne of Tarth",
+            image: "assets/images/brienne.jpg"
+        },
+        "ramsay": {
+            name: "Ramsay Bolton",
+            image: "assets/images/ramsay.jpg"
+        },
+        "gilly": {
+            name: "Gilly",
+            image: "assets/images/gilly.jpg"
+        },
+        "daario": {
+            name: "Daario Naharis",
+            image: "assets/images/daario.jpg"
+        },
+        "missandei": {
+            name: "Missandei",
+            image: "assets/images/missandei.webp"
+        },
+        "ellaria": {
+            name: "Ellaria Sand",
+            image: "assets/images/ellaria.jpg"
+        },
+        "tommen": {
+            name: "Tommen Baratheon",
+            image: "assets/images/tommen.jpg"
+        },
+        "jaqen": {
+            name: "Jaqen H'ghar",
+            image: "assets/images/jaqen.jpg"
+        },
+        "roose": {
+            name: "Roose Bolton",
+            image: "assets/images/roose.jpg"
+        },
+        "sparrow": {
+            name: "The High Sparrow",
+            image: "assets/images/sparrow.jpg"
+        },
+
+
+
+
     }
 
+    //copies the characters object so we can manipulate the keys in it and not affect the original
+    var chosenPeopleObject = JSON.parse(JSON.stringify(characters));
+
     //creates the character cards from the characters object to put into the HTML
-    function createCharactersDiv (character, key) {
-        var charDiv = $("<div class='character' data-name='" + key + "'>");
+    function createCharactersDiv(character, characterIndex) {
+        var charDiv = $("<div class='character' data-name='" + characterIndex + "'>");
         var charName = $("<div class='characterName'>").text(character.name);
         var charImage = $("<img alt='image' class='characterImage'>").attr('src', character.image);
         charDiv.append(charName).append(charImage);
+        //remove the current character from the object so it does not have chance to repeat in next loop
+        delete chosenPeopleObject[characterIndex];
+
         return charDiv;
     }
 
-    // shows all characters to choose from by populating html with created characters
-    function showCharacters() { 
-        var characterArray = Object.keys(characters);
-        for (var i = 0; i < characterArray.length; i++) {
-        var characterIndex = characterArray[i];
-        var character = characters[characterIndex];
-        var charDiv = createCharactersDiv(character, characterIndex);
-        $('#charactersDiv').append(charDiv);
+    // shows a certain number of characters to choose from by populating html with created character cards
+    function showCharacters() {
+        //reset user choices for characters once the object is empty (all choices displayed and removed)
+        if (Object.keys(chosenPeopleObject).length === 0) {
+            chosenPeopleObject = JSON.parse(JSON.stringify(characters));
+        }
+        $('#charactersDiv').empty();
+        for (var i = 0; i < 5 && i <= Object.keys(chosenPeopleObject).length; i++) {
+            console.log(i);
+            console.log(Object.keys(chosenPeopleObject).length);
+            //turn object into array so we can index it with random number
+            var characterArray = Object.keys(chosenPeopleObject);
+            var characterIndex = characterArray[(Math.floor(Math.random() * characterArray.length))];
+            var character = chosenPeopleObject[characterIndex];
+            var charDiv = createCharactersDiv(character, characterIndex);
+            $('#charactersDiv').append(charDiv);
         }
     }
-    
-    //click event for user pressing start button
-    $("#startButton").on("click", function() {
-        showCharacters();
-        $("#startButton").hide();
-    });
 
-    //click event for user pressing a character picture
-    $("#charactersDiv").on("click", ".character", function() {
-        var chosenCharacter = $(this).attr("data-name");
-        console.log("this", this);
-
-        // var queryURL = "https://got-quotes.herokuapp.com/quotes?char=tyrion";
-        var queryURL = "https://got-quotes.herokuapp.com/quotes?char=" + chosenCharacter + "";
-        console.log(queryURL);
+    //query the Game of Thrones Quote API to get a quote by the chosen character
+    function fetchQuote(queryURL) {
         $.ajax({
             url: queryURL,
             method: "GET"
         })
             // After data comes back from the request
-            .then(function(response) {
-                console.log(queryURL);
-                console.log(response);
+            .then(function (response) {
                 // storing the data from the AJAX request in the results variable
-            //   var results = response.data;
-            var quoteString = response.quote + " ~ " + response.character;
-            $("#quote").text(quoteString);
-            addToTable(quoteString);
+                var fetchedQuote = response.quote + " ~ " + response.character;
+                $("#quote").text(fetchedQuote);
+                addToTable(fetchedQuote);
+
+                uri = "https://api.funtranslations.com/translate/dothraki.json?text=" + encodeURIComponent(fetchedQuote);
             });
-    });
-    function addToTable(q, t){
+    }
+
+    //query the Dothraki translator API to translate English text
+    function fetchTranslation() {
+        $.ajax({
+            url: uri,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                // storing the data from the AJAX request in the results variable
+                var translatedQuote = response.contents.translated;
+                console.log(translatedQuote);
+                $("#translated").text(translatedQuote);
+                addToTable(translatedQuote);
+            });
+    }
+
+    // function fetchVideos() {
+    //         var queryURL = 
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     })
+    //         // After data comes back from the request
+    //         .then(function(response) {
+    //         // storing the data from the AJAX request in the results variable
+    //         var videoResults = response.contents.translated;
+    //         console.log(translatedQuote);
+    //         $("#translated").text(translatedQuote);
+    //         addToTable(translatedQuote);
+    //         });
+    // }
+
+    function addToTable(q, t) {
         var quote = q;
         var translation = t;
 
@@ -135,18 +299,54 @@ $(document).ready(function(){
         $("tbody").prepend(newRow);
     }
 
-
-    $("#charactersDiv").on("click", function(event) {
-       
+    //click event for user search (stored in Firebase)
+    $("#searchButton").on("click", function (event) {
         event.preventDefault();
+        var userSearch = $("#searchInput").val().trim();
 
-        logCharacter = $("#charactersDiv").val().trim();
+        // Creates local "temporary" object for holding user search data
+        var newSearch = {
+            searchterm: newSearch
+        };
 
-        database.ref().push({
-            logCharacter: logCharacter,
-            
-        })
-        console.log(logCharacter)
+        database.ref().push(userSearch);
+        $("#searchInput").val("");
     });
-  
+
+    //Database event for retrieving user search terms from Firebase and displaying them as recent searches in HTML
+    database.ref().on("child_added", function (childSnapshot) {
+        var userSearch = childSnapshot.val().searchterm;
+
+    });
+
+    //click event for user pressing people button
+    $("#peopleButton").on("click", function () {
+        showCharacters();
+        $(".row").show();
+    });
+
+    //click event for user pressing translator button
+    $(document).on("click", "#translateButton", function (event) {
+        event.preventDefault();
+        var translateText = $("#translateInput").val().trim();
+        uri = "https://api.funtranslations.com/translate/dothraki.json?text=" + encodeURIComponent(translateText);
+        fetchTranslation(uri);
+        $(".row").show();
+        $("#translateModal").modal("hide");
+    });
+
+    //click event for user pressing a character picture
+    $("#charactersDiv").on("click", ".character", function () {
+
+        var chosenCharacter = $(this).attr("data-name");
+        var queryURL = "https://got-quotes.herokuapp.com/quotes?char=" + chosenCharacter;
+
+        fetchQuote(queryURL);
+
+        fetchTranslation(uri);
+
+        addToTable()
+
+    });
+
 });
